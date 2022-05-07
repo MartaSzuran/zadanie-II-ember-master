b9fb03ec51a69bfc3e297d5cabfd490bfa67219b
MartaSzuran <marta.szuran@gmail.com>
2022-05-07 14:38:32 +0200

    Adding_show_user_info

--- app/application/route.js
+++ app/application/route.js
@@ -5,18 +5,20 @@ export default class ApplicationRoute extends Route {
   @service store;
 
   async beforeModel() {
-    window.localStorage.clear();
-
     const user1 = {
+      id: 1,
       username: 'admin',
       password: 'admin123',
       email: 'admin@admin.com',
       isAdmin: true,
+      photoURL: 'https://d-art.ppstatic.pl/kadry/k/r/e3/2e/5ee3783889b3d_o_large.jpg',
     };
     const user2 = {
+      id: 2,
       username: 'user',
       password: 'user123',
       email: 'user@user.com',
+      photoURL: 'https://d-art.ppstatic.pl/kadry/k/r/68/7d/5ee36919e4c79_o_large.jpg',
     };
 
     const user1Model = this.store.createRecord('user', user1);
@@ -26,16 +28,19 @@ export default class ApplicationRoute extends Route {
     await user2Model.save();
 
     const post1 = {
+      id: 1,
       title: 'Testowy 1',
       body: 'Zawartość testowa 1',
       owner: user1Model,
     };
     const post2 = {
+      id: 2,
       title: 'Testowy 2',
       body: 'Zawartość testowa 2',
       owner: user1Model,
     };
     const post3 = {
+      id: 3,
       title: 'Testowy 3',
       body: 'Zawartość testowa 3',
       owner: user2Model,
--- app/user/model.js
+++ app/user/model.js
@@ -4,6 +4,7 @@ export default class UserModel extends Model {
   @attr('string') username;
   @attr('string') password;
   @attr('string') email;
+  @attr('string') photoURL;
   // two ways of createing default values
   @attr('boolean', { defaultValue: false }) isDeleted;
   @attr('boolean', {
