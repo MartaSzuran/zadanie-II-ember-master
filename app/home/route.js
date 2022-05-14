1664ac38c385d256c2ab6f87098249186a6e24a6
MartaSzuran <marta.szuran@gmail.com>
2022-05-14 12:23:31 +0200

    Adding_logic_to_login_and_logout

--- app/home/route.js
+++ app/home/route.js
@@ -2,14 +2,14 @@ import Route from '@ember/routing/route';
 import { storageFor } from 'ember-local-storage';
 import { inject as service } from '@ember/service';
 
-export default class LoginRoute extends Route {
+export default class HomeRoute extends Route {
   @service router;
   @storageFor('logged-as') loggesAs;
 
   beforeModel() {
     const userId = this.loggesAs.get('id');
-    if (userId) {
-      this.router.transitionTo('home');
+    if (!userId) {
+      this.router.transitionTo('/login');
       return;
     }
   }
