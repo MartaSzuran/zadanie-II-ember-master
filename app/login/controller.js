6c3a7b34a1d352a1e3138e4a36c8dc708996a9e0
MartaSzuran <marta.szuran@gmail.com>
2022-05-14 11:02:22 +0200

    Handle_user_login

--- app/login/controller.js
+++ app/login/controller.js
@@ -1,19 +1,31 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+
 
 export default class LoginController extends Controller {
+  @service store 
+
+  @tracked loginValue
+  @tracked passwordValue
+
   @action 
-  onLoginChange () {
-    console.log('onLoginChange triggered');
+  onLoginChange (event) {
+    this.loginValue =  event.target.value;
   }
 
   @action 
-  onPasswordChange () {
-    console.log('onPasswordChange triggered');
+  onPasswordChange (event) {
+    this.passwordValue =  event.target.value;
   }
-  
+
   @action 
-  onSubmit () {
+  async onSubmit (event) {
+    event.preventDefault();
     console.log('onSubmit triggered');
+    const users = await this.store.query('user', {
+      filter: {username: this.loginValue, password: this.passwordValue}});
+    console.log(users.length);
   }
 }
