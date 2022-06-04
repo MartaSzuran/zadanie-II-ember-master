0a9dc56ed5bfe1cb2d60ac74267bc4f0f98a7e6e
MartaSzuran <marta.szuran@gmail.com>
2022-06-04 10:47:43 +0200

    Adding_logic_to_likes_and_dislikes

--- app/components/posts/list/item/component.js
+++ app/components/posts/list/item/component.js
@@ -1,14 +1,41 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class PostsListItemComponent extends Component {
   @service session;
+  @service store;
+
+  get currentUser() {
+    return this.session.currentUser;
+  }
+
+  get currentPost() {
+    return this.args.post;
+  }
+
+  @action
+  async onLike() {
+    const createdLike = this.store.createRecord('like', {
+      user: this.currentUser,
+      post: this.currentPost,
+    });
+    await createdLike.save();
+    // console.log('like');
+  }
+
+  @action
+  async onDislike() {
+    const like = this.currentUser.likes.find((like) => {
+      return like.post.get('id') === this.currentPost.id;
+    });
+    await like.destroyRecord();
+    // console.log('dislike');
+  }
 
   get isAlreadyLikedByCurrentUser() {
-    const { currentUser } = this.session;
-    const { post } = this.args;
-    const likeModel = currentUser.likes.find((like) => {
-      return like.post.get('id') === post.id;
+    const likeModel = this.currentUser.likes.find((like) => {
+      return like.post.get('id') === this.currentPost.id;
     });
     const isLikeExists = Boolean(likeModel);
     return isLikeExists;
