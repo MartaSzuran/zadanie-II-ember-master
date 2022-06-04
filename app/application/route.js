d70598e36e300b00189b8ddd489207fb2517e155
MartaSzuran <marta.szuran@gmail.com>
2022-06-04 16:56:47 +0200

    Storing_likes

--- app/application/route.js
+++ app/application/route.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { scheduleOnce } from '@ember/runloop';
 
 export default class ApplicationRoute extends Route {
   @service store;
@@ -64,5 +65,12 @@ export default class ApplicationRoute extends Route {
 
     const like1Model = this.store.createRecord('like', like1);
     await like1Model.save();
+
+    this._fixLikeRelationship();
+  }
+
+  _fixLikeRelationship() {
+    const fetchLikes = async () => this.store.findAll('like');
+    scheduleOnce('afterRender', this, fetchLikes);
   }
 }
