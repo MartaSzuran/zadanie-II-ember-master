fd572fa38a0e98782118026db85dec3695b11cb4
MartaSzuran <marta.szuran@gmail.com>
2022-06-11 11:25:42 +0200

    Adding_date_attr_to_post_model

--- app/post/model.js
+++ app/post/model.js
@@ -5,6 +5,16 @@ export default class PostModel extends Model {
   @attr('string') title;
   @attr('string') body;
   @attr('boolean', { defaultValue: false }) isDeleted;
+  @attr('date', { defaultValue: () => new Date() }) createdAt;
   @belongsTo('user', { autoSave: true }) owner;
   @hasMany('like') likes;
+
+  get fullDate() {
+    const date = new Date(this.createdAt);
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    const dateString = `${day}-${month}-${year}`;
+    return dateString;
+  }
 }
