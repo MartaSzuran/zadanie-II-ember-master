047ebd1a11496f7a1c0ce37d8dc3b3d6678c0967
MartaSzuran <marta.szuran@gmail.com>
2022-07-02 16:23:40 +0200

    Fix_facotry_to_generate_random_users_if_we_need_more_users

--- mirage/factories/user.js
+++ mirage/factories/user.js
@@ -2,8 +2,19 @@ import { Factory } from 'ember-cli-mirage';
 import { faker } from '@faker-js/faker';
 
 export default Factory.extend({
-  username: faker.name.firstName(),
-  password: faker.internet.password(),
-  email: faker.internet.email(),
-  photoURL: faker.image.avatar(),
+  username() {
+    return faker.name.firstName();
+  },
+
+  password() {
+    return faker.internet.password();
+  },
+
+  email() {
+    return faker.internet.email();
+  },
+
+  image() {
+    return faker.image.avatar();
+  },
 });
